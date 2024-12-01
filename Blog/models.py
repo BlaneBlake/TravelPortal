d4628148f1c8afbd0f5ee88476b5357be76fbd6b
Bogdan Anczarski <bogdan@SuperKomputer.L440>
2024-12-01 22:12:38 +0100

    tags parameter blank is set as True in models

--- Blog/models.py
+++ Blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
 
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
     location_url = models.URLField(max_length=500, null=True, blank=True)
     place_name = models.CharField(max_length=200, null=True, blank=True)
     estimated_time = models.DurationField(null=True, blank=True)
